0873e0e6f6ca8d5d4c249b9e58ede9e3d09662b4
anani anatide <ananianatid@gmail.com>
2025-09-11 08:30:58 +0000

    Comment out foreign key constraint for 'specialite_id' in enseignants table migration to facilitate future updates.

---
+++ database/migrations/2025_09_11_082052_create_etudiants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('user')->onDelete('null');
+            $table->foreignId('promotion_id')->constrained('promotions')->onDelete('null');
+            $table->string('matricule')->nullable()->unique();
+            $table->date('naissance');
+            // $table->date('inscripton');
+            $table->date('graduatin')->nullable();
+            $table->string('parent')->nullable();
+            $table->string('telephone_parent')->nullable();
+            $table->string('telephone_parent')->nullable();
+            $table->enum('statut',['actif','diplome','suspendu','abandon']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('etudiants');
+    }
+};
