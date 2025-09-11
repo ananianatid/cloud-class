96461ef901aa6f4d5ebf9652ac5edb373e0545b4
anani anatide <ananianatid@gmail.com>
2025-09-11 07:18:29 +0000

    Update users table migration to include French field names and additional columns for telephone, address, role, gender, and photo.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('telephone')->nullable();
+            $table->string('adresse')->nullable();
+            $table->enum('role', ["etudiant","enseignant","administrateur"]);
+            $table->enum('sexe', ["M","F"]);
+            $table->string('photo');
             $table->rememberToken();
             $table->timestamps();
         });
